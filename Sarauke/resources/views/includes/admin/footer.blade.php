3bbf6b10fa7f68e2b919f67c95aae4a54e561d4f
Difkiwahyudi <wahyudidifki22@gmail.com>
2024-05-10 22:14:18 +0700

    no message

--- Sarauke/resources/views/includes/admin/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- Footer -->
-<footer class="sticky-footer bg-white">
-  <div class="container my-auto">
-    <div class="copyright text-center my-auto">
-      <span>Sarauke Travel Admin Page</span>
-    </div>
-  </div>
-</footer>
-<!-- End of Footer -->
