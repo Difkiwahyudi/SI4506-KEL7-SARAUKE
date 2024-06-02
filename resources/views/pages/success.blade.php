9a133422a75cc01e661fc0f57a1678c86011511f
anandadjati <anandaaisha30@gmail.com>
2024-06-02 17:00:45 +0700

    KEL7-33

--- resources/views/pages/success.blade.php
+++ resources/views/pages/success.blade.php
@@ -7,11 +7,6 @@
     <div class="col text-center">
       <img src="{{ url('frontend/images/ic_mail.png') }}" alt="" />
       <h1>Yay! Success</h1>
-      <p>
-        We’ve sent you email for trip instruction
-        <br />
-        please read it as well
-      </p>
       <a href="{{ url('/') }}" class="btn btn-home-page mt-3 px-5">
         Home Page
       </a>
